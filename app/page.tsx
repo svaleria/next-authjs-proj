77bf295ce991052cc3a6e336041fc17f7d199709
unknown <svaleria.gonzales@gmail.com>
2024-10-26 23:55:52 -0400

    simple login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
+import { Inter } from "next/font/google";
+
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
+
+const font = Inter({
+  subsets: ["latin"],
+  weight: ["600"],
+})
 
 export default function Home() {
   return (
-    <Button size="lg" >Click me!!!</Button>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-pink-500 to-purple-700">
+      <div className="space-y-6 text-center">
+        <h1 className={cn(
+          "text-6xl font-semibold text-white drop-shadow-md",
+          font.className          
+          )}>
+          Auth
+        </h1>
+        <p className="text-white text-lg">
+          Simple Authentication Service
+        </p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
