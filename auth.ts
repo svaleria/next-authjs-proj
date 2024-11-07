f4ce6576a31c0302837f79338f02b36e4e10ae1f
unknown <svaleria.gonzales@gmail.com>
2024-11-06 20:38:31 -0400

    middleware and auth.config

--- auth.ts
+++ auth.ts
@@ -1,7 +1,20 @@
+// import NextAuth from "next-auth"
+// import GitHub from "next-auth/providers/github"
+// import Google from "next-auth/providers/google"
+ 
+// export const { auth, handlers, signIn, signOut } = NextAuth({
+//   providers: [GitHub, Google],
+// })
+
 import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+import authConfig from "./auth.config"
+ 
+const prisma = new PrismaClient()
  
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [GitHub, Google],
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  ...authConfig,
 })
---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest } from "next/server"
+import authConfig from "./auth.config"
+import NextAuth from "next-auth"
+ 
+// Use only one of the two middleware options below
+// 1. Use middleware directly
+// export const { auth: middleware } = NextAuth(authConfig)
+ 
+// 2. Wrapped middleware option
+const { auth } = NextAuth(authConfig)
+export default auth(async function middleware(req: NextRequest) {
+  // Your custom middleware logic goes here
+  console.log("ROUTE: ", req.nextUrl.pathname)
+})
+
+export const config = {
+    matcher: [
+        // Skip Next.js internals and all static files, unless found in search params
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        // Always run for API routes
+        '/(api|trpc)(.*)',
+      ],
+}
