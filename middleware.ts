dc28610c070cda73bf5c1de0751c32b84bb7cd99
unknown <svaleria.gonzales@gmail.com>
2024-11-06 21:44:16 -0400

    middleware updated

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,36 @@
 import { NextRequest } from "next/server"
 import authConfig from "./auth.config"
 import NextAuth from "next-auth"
+// import { auth } from "./auth"
+
+// type NextAuthRequest = {
+//     headers: Headers
+//     url: string
+//     auth?: unknown
+//   }
  
 // Use only one of the two middleware options below
 // 1. Use middleware directly
 // export const { auth: middleware } = NextAuth(authConfig)
  
+// // 2. Wrapped middleware option
+// const { auth } = NextAuth(authConfig)
+
+// export default auth(async function middleware(req: NextAuthRequest) {
+//   // Your custom middleware logic goes here
+//   const isLoggedIn = !!req.auth;
+//   console.log("ROUTE: ", req.url);
+//   console.log("IS LOGGEDIN: " , isLoggedIn);
+// })
+
 // 2. Wrapped middleware option
 const { auth } = NextAuth(authConfig)
+
 export default auth(async function middleware(req: NextRequest) {
   // Your custom middleware logic goes here
-  console.log("ROUTE: ", req.nextUrl.pathname)
+  const isLoggedIn = !!req.nextUrl.username;
+  console.log("ROUTE: ", req.nextUrl.pathname);
+  console.log("IS LOGGEDIN: " , isLoggedIn);
 })
 
 export const config = {
